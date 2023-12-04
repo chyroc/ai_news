f4ab72049c00f01434b609bde81b5f416df24e11
chyroc <chyroc@qq.com>
2023-12-04 14:17:10 +0800

    twitter reddit

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/chyroc/ai_news
 
 go 1.19
+
+require (
+	github.com/chyroc/anyhow v0.1.3 // indirect
+	github.com/chyroc/persistent-cookiejar v0.1.0 // indirect
+	github.com/chyroc/requests v0.0.0-20231203033829-61667603cd1e // indirect
+	golang.org/x/net v0.19.0 // indirect
+)
--- script/main.go
+++ script/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/chyroc/requests"
+)
+
+const output = "./docs"
 
 func main() {
 	fmt.Println("start")
+	assert(
+		twitter("OpenAI"),
+		twitter("GoogleAI"),
+		twitter("LangChainAI"),
+
+		reddit("LangChain"),
+	)
+}
+
+func twitter(username string) error {
+	output := output + "/twitter/" + username + ".xml"
+	url := fmt.Sprintf("https://rsshub.app/twitter/user/%s", username)
+	if err := os.MkdirAll(filepath.Dir(output), os.ModePerm); err != nil {
+		return fmt.Errorf("twitter mkdir failed: %s", err)
+	}
+	text := requests.Get(url).Text()
+	if text.IsErr() {
+		return text.Err()
+	}
+	return ioutil.WriteFile(output, []byte(text.Value()), 0644)
+}
+
+func reddit(username string) error {
+	output := output + "/reddit/" + username + ".xml"
+	url := fmt.Sprintf("https://www.reddit.com/r/%s.rss", username)
+	if err := os.MkdirAll(filepath.Dir(output), os.ModePerm); err != nil {
+		return fmt.Errorf("reddit mkdir failed: %s", err)
+	}
+	text := requests.Get(url).Text()
+	if text.IsErr() {
+		return text.Err()
+	}
+	return ioutil.WriteFile(output, []byte(text.Value()), 0644)
+}
+
+func assert(err ...error) {
+	for _, v := range err {
+		if v != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+	}
 }
